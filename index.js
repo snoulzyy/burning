59ebd931399d3449351d8312269877e893752a10
snoulzyy <danishkwek888@gmail.com>
2026-09-08 03:28:02 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ const REGIONS = [
   { id: "GOB1", area: "GOB", enabled: true, levels: [298, 299] },
   { id: "GOB4", area: "GOB", enabled: true, levels: [298, 299] },
   { id: "GOB5", area: "GOB", enabled: true, levels: [298, 299] },
+  { id: "GOB6", area: "GOB", enabled: true, levels: [298, 299] },
   { id: "GOB7", area: "GOB", enabled: true, levels: [298, 299] },
   { id: "GOB8", area: "GOB", enabled: true, levels: [298, 299] }
 ];
